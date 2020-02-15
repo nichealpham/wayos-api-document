313bdd9faa4ab5c887d73a603449a498043e4575
Nicheal <nichealpham@gmail.com>
2020-02-16 00:28:10 +0700

    Add document for staff + company

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const app = express();
 const swaggerUi = require('swagger-ui-express');
 
 // document list
-const documentAuthentication = yaml.safeLoad(fs.readFileSync('./swaggers/authentication.yaml', 'utf8'));
+const documentAuthentication = yaml.safeLoad(fs.readFileSync('./swaggers/Authentication.yaml', 'utf8'));
+const documentStaff = yaml.safeLoad(fs.readFileSync('./swaggers/Staff.yaml', 'utf8'));
+const documentCompany = yaml.safeLoad(fs.readFileSync('./swaggers/Company.yaml', 'utf8'));
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(documentAuthentication));
-app.use('/authentication', swaggerUi.serve, swaggerUi.setup(documentAuthentication));
+app.use('/authentication', swaggerUi.serve, (req, res) => res.send(swaggerUi.generateHTML(documentAuthentication)));
+app.use('/staff', swaggerUi.serve, (req, res) => res.send(swaggerUi.generateHTML(documentStaff)));
+app.use('/company', swaggerUi.serve, (req, res) => res.send(swaggerUi.generateHTML(documentCompany)));
 
 app.listen(3200, () => console.log("Document server started on port 3200"));
