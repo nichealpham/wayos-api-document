a9d525bba54567ba81dcb94290e813a547515c84
Nicheal <nichealpham@gmail.com>
2020-02-16 12:02:31 +0700

    Add custom css

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ const express = require('express');
 const app = express();
 const swaggerUi = require('swagger-ui-express');
 
+const options = {
+    explorer: true,
+    customCssUrl: '/css/global.css',
+};
+
 // document list
 const documentAuthentication = yaml.safeLoad(fs.readFileSync('./swaggers/Authentication.yaml', 'utf8'));
 const documentStaff = yaml.safeLoad(fs.readFileSync('./swaggers/Staff.yaml', 'utf8'));
 const documentCompany = yaml.safeLoad(fs.readFileSync('./swaggers/Company.yaml', 'utf8'));
 
-app.use('/authentication', swaggerUi.serve, (req, res) => res.send(swaggerUi.generateHTML(documentAuthentication)));
-app.use('/staff', swaggerUi.serve, (req, res) => res.send(swaggerUi.generateHTML(documentStaff)));
-app.use('/company', swaggerUi.serve, (req, res) => res.send(swaggerUi.generateHTML(documentCompany)));
+app.use(express.static('public'));
+
+app.use('/authentication', swaggerUi.serve, (req, res) => res.send(swaggerUi.generateHTML(documentAuthentication, options)));
+app.use('/staff', swaggerUi.serve, (req, res) => res.send(swaggerUi.generateHTML(documentStaff, options)));
+app.use('/company', swaggerUi.serve, (req, res) => res.send(swaggerUi.generateHTML(documentCompany, options)));
 
 app.listen(3200, () => console.log("Document server started on port 3200"));
